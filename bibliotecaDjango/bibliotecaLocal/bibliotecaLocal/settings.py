fe122bfb0f625a978f4b038bca71c165144d3ec0
Leandro Miranda <leandrobeirao@gmail.com>
2023-01-31 23:33:15 -0300

    Atualização de configurações

--- bibliotecaDjango/bibliotecaLocal/bibliotecaLocal/settings.py
+++ bibliotecaDjango/bibliotecaLocal/bibliotecaLocal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #nova aplicação
     'catalog.apps.CatalogConfig',
+    
 ]
 
 MIDDLEWARE = [
--- bibliotecaDjango/bibliotecaLocal/catalog/models.py
+++ bibliotecaDjango/bibliotecaLocal/catalog/models.py
@@ -1,3 +1,34 @@
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Genero(models.Model):
+    """Modelo representando um genero literario"""
+
+    nome = models.CharField(max_length=200, help_text='Digite um genereo. Ex: Ficção científica')
+
+    def __str__(self):       
+        
+        return self.name
+
+class Livro(models.Model):
+
+    titulo = models.CharField(max_length=200)
+    #autor = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    sumario = models.TextField(max_length=1000, help_text='Digite uma descrição resumida do livro')
+    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='código de 13 caracteres ISBN - href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genero = models.ManyToManyField(Genero, help_text='Selecione um genero para este livro')
+
+    def __str__(self):
+        
+        return self.titulo
+
+    def get_absolute_url(self):
+
+        return reverse('book-detail', args=[str(self.id)])
+
+        
+
