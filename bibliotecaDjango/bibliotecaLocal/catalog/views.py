e6ce1fadf063d2916203212265234c37a41215e7
Leandro Miranda <leandrobeirao@gmail.com>
2023-02-16 22:13:25 -0300

    login e autenticação de usuário iniciados

--- bibliotecaDjango/bibliotecaLocal/catalog/views.py
+++ bibliotecaDjango/bibliotecaLocal/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Livro,Autor,InstanciaLivro,Genero
+from django.views import generic
 
 # Create your views here.
 
@@ -11,6 +12,9 @@ def index(request):
     num_autores = Autor.objects.count()
     num_generos = Livro.objects.all().count()
     num_ensaio = Livro.objects.filter(titulo__icontains = 'ensaio').count()
+    num_visitas = request.session.get('num_visitas',0)
+
+    request.session['num_visitas'] = num_visitas +1
 
     context = {
         'num_livros': num_livros,
@@ -19,24 +23,21 @@ def index(request):
         'num_autores': num_autores,
         'num_generos': num_generos,
         'num_ensaio': num_ensaio,
+        'num_visitas': num_visitas,
     }
 
     return render(request, 'index.html', context=context)
 
-
-from django.views import generic
-
 class LivroListView(generic.ListView):
-    model = Livro 
-        
+    model = Livro     
 
 class LivroDetalheView(generic.DetailView):
     model = Livro
+    paginate_by = 10
      
-
 class AutorListView(generic.ListView):
     model = Autor
 
-
 class AutorDetalheView(generic.DetailView):
     model = Autor
+
